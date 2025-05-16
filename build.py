77d382bde1e90fd0c574bebb4e8a948d2c7e01b5
IsaacVega5 <isaacvega361@gmail.com>
2025-05-16 12:12:01 -0400

    [build] Update build file

--- build.py
+++ build.py
@@ -1,11 +1,17 @@
 import PyInstaller.__main__
 import PyInstaller.config
+import shutil
+import os
 
 PyInstaller.__main__.run([
   "main.py", 
   "--name", "InfoCuboFunctionality", 
-  "--onefile", 
   "--noconsole", 
   "--icon=cube.ico",
-  "--version-file", "version.txt"                        
+  "--version-file", "version.txt" 
 ])
+
+#COPYING INDEX TABLE
+if not os.path.exists("dist/InfoCuboFunctionality/data/"):
+  os.makedirs("dist/InfoCuboFunctionality/data/")
+shutil.copyfile("data/indexTable.csv", "dist/InfoCuboFunctionality/data/indexTable.csv")
